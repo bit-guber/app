52206d8b5c73f71d79318c9a9039b7d06e475b08
bit_guber <mgrguber@gmail.com>
2024-06-18 16:25:55 +0530

    fixed paths and change stucture

--- api/index.py
+++ api/index.py
@@ -1,18 +1,36 @@
-from flask import Flask, request, Response
+from flask import Flask, request, Response, redirect
+import requests as SendRequesttoWeb
 try:
     from music import *
 except:
     from api.music import *
 app = Flask(__name__)
+session = SendRequesttoWeb.session()
+jioSaavan_req_headers = { "User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:109.0) Gecko/20100101 Firefox/119.0" }
 
+@app.route( "/getSongName", methods = [ 'POST' ] )
+def get_song_name():
+    try:
+        return getSongTitle(request.json)
+    except KeyError as e:
+        print(e)
+        return Response( '', status=401 )
 
-# @app.route( "/get-list", methods = ['POST'] )
-# def get_recommmendedMovies( ):
-#     return get_list(request=request)
-	
-# @app.route( "/get_auth" )
-# def get_token():
-#      return {'token':"eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI4ODVhOGY3NGUxYjFhNjc0YTIwZDhmY2UzYzQ3OWJiOSIsInN1YiI6IjY1MmU3NTcxMDI0ZWM4MDExZTM1Njk5NSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.cY6LOPoZCTF5fuZ7h42N_bqHqau3XxgrHUI2o37Fvto"}
+@app.route( "/getSong", methods = ['POST'] )
+def get_song():
+    try:
+        name, url = getSongPath( request.json )
+        res = session.get( url, headers=jioSaavan_req_headers )
+        if res.ok:
+            SongRes = redirect( res.json()['auth_url'] )
+            SongRes.headers.set( "Song-Name", name )
+            return SongRes
+        else:
+            return Response( '', status=402 )
+    except KeyError as e:
+        print(e)
+        return Response( '', status=401 )
+
 @app.route( "/playlistSongs", methods = [ 'POST' ] )
 def get_PlaylistSongs():
     try:
@@ -25,10 +43,11 @@ def get_PlaylistSongs():
 def get_samplePlaylists():
     return getSampler()
 
-@app.route("/test", methods = [ 'POST', 'GET' ])
-def test():
-    print( request.json, request.json.keys() )
-    return "<p>j<p>"
+@app.route( "/getMusicImage", methods=['POST']  )
+def getMusicImage():
+    res = request.json
+    return getImage( res['t'], res['id'], res['s'] )
+
 
 @app.route("/")
 def hello_world():
--- api/music.py
+++ api/music.py
@@ -1,6 +1,8 @@
 import json
 import random
-from flask import Response
+
+
+from flask import Response, redirect
 
 path = "resources/"
 totalSongSize = 5
@@ -8,7 +10,11 @@ songs = json.load( open(path + 'songs.json', 'r') )
 playlists = json.load( open(path + 'playlists.json', 'r') )
 artists = json.load( open(path + 'artists.json', 'r') )
 
+def getSongTitle(body):
+    return songs[ body['id'] ]['t']
 
+def getSongPath( body ):
+    return songs[body['id']]['t'], f"https://www.jiosaavn.com/api.php?__call=song.generateAuthToken&url="+songs[body['id']]['emu']+"&bitrate="+body['b']+"&api_version=4&_format=json&ctx=web6dot0&_marker=0"
 
 def getPlaylistSongs( body ):
     temp = dict()
@@ -16,10 +22,19 @@ def getPlaylistSongs( body ):
         currentPlaylist = playlists[ str(body['id']) ]
     except:
         return Response( status=210 )
-    index = int(body['np'])*totalSongSize                           # npage * limitSize
-    temp['lp'] = index+totalSongSize < len( currentPlaylist['g'] )   # last_page
-    temp['data'] = list( map( songWrapper, currentPlaylist['g'][ index:totalSongSize+index ] ) )
+    temp['t'] = currentPlaylist['t']
+    temp['data'] = currentPlaylist['g']
     return temp
+# def getPlaylistSongs( body ):
+#     temp = dict()
+#     try:
+#         currentPlaylist = playlists[ str(body['id']) ]
+#     except:
+#         return Response( status=210 )
+#     index = int(body['np'])*totalSongSize                           # npage * limitSize
+#     temp['lp'] = index+totalSongSize < len( currentPlaylist['g'] )   # last_page
+#     temp['data'] = list( map( songWrapper, currentPlaylist['g'][ index:totalSongSize+index ] ) )
+#     return temp
 
 def songWrapper(_id):
     temp = dict()
@@ -29,6 +44,11 @@ def songWrapper(_id):
     temp['u'] = songs[_id]['emu']
     return temp
 
+def getImage(type:str, _id:str, size:str):
+    if type == "s":
+        return redirect( songs[_id]['i'].replace( "50x50", f'{size}x{size}' ) )
+    else:
+        return redirect( playlists[_id]['i'].replace( "50x50", f'{size}x{size}' ) )
 def playlistWrapper(_id):
     temp = dict()
     temp['id'] = _id
@@ -40,5 +60,5 @@ def getSampler():
     temp = { 'data':[] }
     for name in [ 'hits', 'latest', 'best' ]:
         keys = random.sample( list(playlists.keys()), 5 )
-        temp['data'].append(  { name: list(map( playlistWrapper, keys )) }  )
+        temp[name] = keys
     return temp 
